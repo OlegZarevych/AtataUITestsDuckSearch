f4435da8f3b903cda14d197aa5c76dcff5aa9a43
Oleg <oleg.zarevych@gmail.com>
2020-02-05 02:49:52 +0200

    initial commit

---
+++ DuckSearch.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtataUITestsDuckSearch
+{
+    [TestFixture]
+    public class DuckSearch : UITestFixture
+    {
+        [Test]
+        public void DuckDuckSearch()
+        {
+
+        }
+    }
+}
