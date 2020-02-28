47ed392b6eefe7c3ea4d2a9b5c6a22dea867ae63
Oleg <oleg.zarevych@gmail.com>
2020-02-29 00:30:42 +0200

    add aditional waiting

--- PageObjects/DuckDuckSearchResult.cs
+++ PageObjects/DuckDuckSearchResult.cs
@@ -5,5 +5,8 @@ namespace AtataUITestsDuckSearch.PageObjects
     [Url("/?q=")]
     public class DuckDuckSearchResult : Page<DuckDuckSearchResult>
     {
+        [FindById]
+        [WaitFor(Until.Visible, TriggerEvents.Init)]
+        public Clickable<DuckDuckSearchResult> Header { get; private set; }
     }
 }
